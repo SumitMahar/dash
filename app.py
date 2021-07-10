c795414b9c90edc213b3e48e53e7048c452cafd8
Sumit Mahar <SumitMahar78@gmail.com>
2021-07-10 22:54:05 +0530

    added bs4 Modal in navbar

--- app.py
+++ app.py
@@ -22,6 +22,17 @@ app = dash.Dash(__name__, title="Py-Dash", external_stylesheets=external_stylesh
 # Main layout of the app
 app.layout = html.Div(className="", children=[navbar, layout_1, layout_2])
 
+# Callback function for bootstrap Modal
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("open", "n_clicks"), Input("close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
 
 @app.callback(
     Output("graph1", "figure"),
--- layout.py
+++ layout.py
@@ -4,13 +4,89 @@ from dash_bootstrap_components._components.NavbarSimple import NavbarSimple
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash_html_components.P import P
 import pandas as pd
+
+# Description for bootstrap Modal
+about_dashboard = html.Div(
+    children=[
+        html.A(
+            [html.I(className="fa fa-bar-chart mx-1"), "Data Source (nse)"],
+            href="https://www1.nseindia.com/products/content/equities/equities/eq_security.htm",
+            className="btn btn-outline-info shadow mb-3 mr-2",
+        ),
+        html.A(
+            [html.I(className="fa fa-github mx-1"), "Github Repository"],
+            href="https://github.com/SumitMahar/dash",
+            className="btn btn-dark shadow mb-3",
+        ),
+        html.P(
+            """
+            *The Content on this site is for informational purposes only and to illustrates what is possible using 
+        the Open Source & free Python data visualisation library Plotly Dash.
+        The code is entirely Open Source, and is intended as a showcase of what is possible 
+        in very few lines of python Plotly Dash code. 
+        """,
+            className="text-muted",
+        ),
+        html.P(
+            """
+        This is rapid app development: not intended for 'production', more for prototyping.
+        Think of it as a completely free Tableau, with the power of python for data science
+        & machine learning directly accessible. The source code is in the GitHub link above.
+        """
+        ),
+        html.P(
+            """
+        The dashboard is optimised for desktop or laptop.
+        """
+        ),
+        html.P(
+            """
+        To save any plot locally, use the camera icon in top left. 
+        """
+        ),
+        html.P(
+            """
+        All plots are interactive -hover over charts, lines & points 
+        for tooltips (dynamic annotations), zoom using your mouse (drag to select area to 
+        zoom into), and double click to zoom out & reset. 
+        """
+        ),
+        html.P(
+            """
+        It was rapid to develop, and trivial to host (for free) via Heroku - see the GitHub link for details. 
+        """
+        ),
+    ]
+)
+
+modal = html.Div(
+    [
+        dbc.Button(
+            "About the Dashboard", id="open", n_clicks=0, className="btn btn-info"
+        ),
+        dbc.Modal(
+            [
+                dbc.ModalHeader("Stock Dashboard | NSE Data"),
+                dbc.ModalBody(children=about_dashboard),
+                dbc.ModalFooter(
+                    dbc.Button("Close", id="close", className="ml-auto", n_clicks=0)
+                ),
+            ],
+            id="modal",
+            is_open=False,
+            size="lg",
+        ),
+    ]
+)
 
 # Navbar
 navbar = NavbarSimple(
     className="shadow text-white",
     children=[
-        dbc.NavLink("About the Dashboard", className="btn btn-info text-white"),
+        # dbc.NavLink("About the Dashboard", className="btn btn-info text-white", id='open'),
+        modal,
         dbc.NavLink(
             [
                 html.I(className="fa fa-github mx-1"),
