23e96d151d45c2f6df05bef0868dff707ce93da4
Sumit Mahar <SumitMahar78@gmail.com>
2021-07-13 12:48:03 +0530

    minor changes in the layout

--- layout.py
+++ layout.py
@@ -4,8 +4,14 @@ from dash_bootstrap_components._components.NavbarSimple import NavbarSimple
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash_html_components.P import P
 import pandas as pd
+
+from stockdata import SYMBOLS, MIN_DATE, MAX_DATE
+
+
+# Options for dropdown menu
+# label: user sees | value : script sees
+options = [{"label": tic, "value": tic} for tic in SYMBOLS]
 
 # Description for bootstrap Modal
 about_dashboard = html.Div(
@@ -25,7 +31,8 @@ about_dashboard = html.Div(
             *The Content on this site is for informational purposes only and to illustrates what is possible using 
         the Open Source & free Python data visualisation library Plotly Dash.
         The code is entirely Open Source, and is intended as a showcase of what is possible 
-        in very few lines of python Plotly Dash code. 
+        in very few lines of python Plotly Dash code.
+        Code is available on github.
         """,
             className="text-muted",
         ),
@@ -55,12 +62,14 @@ about_dashboard = html.Div(
         ),
         html.P(
             """
-        It was rapid to develop, and trivial to host (for free) via Heroku - see the GitHub link for details. 
+        See the GitHub link for details.
+        (Reach me at SumitMahar78@gmail.com)
         """
         ),
     ]
 )
 
+# Modal code taken from official dash-bootstrap documentation
 modal = html.Div(
     [
         dbc.Button(
@@ -99,8 +108,8 @@ navbar = NavbarSimple(
             href="https://linkedin.com/in/sumit-mahar",
         ),
     ],
-    brand="Stock Dashboard",
-    brand_href="#",
+    brand="Stock Dashboard ( IT sector )",
+    brand_href="/",
     color="dark",
     dark=True,
 )
@@ -109,31 +118,111 @@ navbar = NavbarSimple(
 layout_1 = dbc.Row(
     className="text-center m-4",
     children=[
+        # First col
         dbc.Col(
             className="card card-body col-md-7 col-sm-12 shadow",
             children=[
-                dcc.Checklist(
-                    id="toggle-rangeslider",
-                    options=[{"label": "Include Rangeslider", "value": "slider"}],
-                    value=["slider"],
+                # Div for dropdown and checklist
+                html.Div(
+                    [
+                        # Div for dropdown field
+                        html.Div(
+                            [
+                                html.P(
+                                    [
+                                        html.I(className="fa fa-line-chart mr-1"),
+                                        "Select a stock symbol",
+                                    ],
+                                    className="d-inline mr-2",
+                                ),
+                                dcc.Dropdown(
+                                    id="ticker",
+                                    options=options,
+                                    value=options[0]["label"],
+                                    className="text-dark",
+                                ),
+                            ],
+                            className="d-inline col-md-6",
+                        ),
+                        # Div for checklist
+                        html.Div(
+                            [
+                                dcc.Checklist(
+                                    id="toggle-rangeslider",
+                                    options=[
+                                        {
+                                            "label": "Include Rangeslider",
+                                            "value": "slider",
+                                        }
+                                    ],
+                                    value=["slider"],
+                                    className="d-inline",
+                                ),
+                            ],
+                            className="d-inline col-md-6",
+                        ),
+                    ],
+                    # class for drowpdown and checklist div
                     className="bg-dark text-white",
                 ),
+                # figure stuff
                 dcc.Graph(
                     id="graph1",
                 ),
             ],
         ),
+        # 2nd col
         dbc.Col(
             className="card card-body col-md-5 col-sm-12 shadow",
             children=[
-                dcc.Checklist(
-                    id="toggle-rangeslider2",
-                    options=[{"label": "Include Rangeslider", "value": "slider"}],
-                    value=["slider"],
+                # Div for dropdown and checklist
+                html.Div(
+                    [
+                        # Div for dropdown field
+                        html.Div(
+                            [
+                                html.P(
+                                    [
+                                        html.I(className="fa fa-line-chart mr-1"),
+                                        "Select stock symbol",
+                                    ],
+                                    className="d-inline mr-2",
+                                ),
+                                # will be able to select multiple stock symbols (will be returned as list to callback function)
+                                dcc.Dropdown(
+                                    id="multi_tickers",
+                                    options=options,
+                                    value=[op["label"] for op in options[:2]],
+                                    className="text-dark",
+                                    multi=True,
+                                ),
+                            ],
+                            className="d-inline col-md-6",
+                        ),
+                        # Div for checklist
+                        html.Div(
+                            [
+                                dcc.Checklist(
+                                    id="toggle-rangeslider2",
+                                    options=[
+                                        {
+                                            "label": "Include Rangeslider",
+                                            "value": "slider",
+                                        }
+                                    ],
+                                    value=["slider"],
+                                    className="d-inline",
+                                ),
+                            ],
+                            className="d-inline col-md-6",
+                        ),
+                    ],
+                    # class for drowpdown and checklist div
                     className="bg-dark text-white",
                 ),
+                # plotly figure (line chart)
                 dcc.Graph(
-                    id="graph2",
+                    id="line_chart",
                 ),
             ],
         ),
@@ -142,33 +231,106 @@ layout_1 = dbc.Row(
 
 # Layout for last 2 figures
 layout_2 = dbc.Row(
-    className="text-center m-4",
+    className="text-center m-4 justify-content-md-center",
     children=[
+        # 1st col
         dbc.Col(
             className="card card-body col-md-6 col-sm-12 shadow",
             children=[
-                dcc.Checklist(
-                    id="toggle-rangeslider3",
-                    options=[{"label": "Include Rangeslider", "value": "slider"}],
-                    value=["slider"],
+                # Div for dropdown options and date picker range
+                html.Div(
+                    [
+                        # Div for dropdown field
+                        html.Div(
+                            [
+                                html.P(
+                                    [
+                                        html.I(className="fa fa-line-chart mr-1"),
+                                        "Select stock symbol",
+                                    ],
+                                    className="d-inline mr-2",
+                                ),
+                                # will be able to select single stock (will be returned as str to callback function)
+                                dcc.Dropdown(
+                                    id="bar_ticker",
+                                    options=options,
+                                    value=options[0]["label"],
+                                    className="text-dark",
+                                ),
+                                # Div for date picker
+                                html.Div(
+                                    [
+                                        dcc.DatePickerRange(
+                                            id="bar_date_picker",
+                                            min_date_allowed=MIN_DATE,
+                                            max_date_allowed=MAX_DATE,
+                                            start_date=MAX_DATE - pd.Timedelta(days=3),
+                                            end_date=MAX_DATE,
+                                            className="d-inline",
+                                        ),
+                                    ],
+                                    className="d-inline col-md-6",
+                                ),
+                            ],
+                            className="d-inline col-md-6",
+                        ),
+                    ],
+                    # class for drowpdown and checklist div
                     className="bg-dark text-white",
                 ),
+                # Pie chart
                 dcc.Graph(
-                    id="graph3",
+                    id="bar_graph",
                 ),
             ],
         ),
+        # 2nd col
         dbc.Col(
             className="card card-body col-md-6 col-sm-12 shadow",
             children=[
-                dcc.Checklist(
-                    id="toggle-rangeslider4",
-                    options=[{"label": "Include Rangeslider", "value": "slider"}],
-                    value=["slider"],
+                html.Div(
+                    [
+                        # Div for dropdown field
+                        html.Div(
+                            [
+                                html.P(
+                                    [
+                                        html.I(className="fa fa-line-chart mr-1"),
+                                        "Select stock symbol",
+                                    ],
+                                    className="d-inline mr-2",
+                                ),
+                                # will be able to select multiple stock symbols (will be returned as list to callback function)
+                                dcc.Dropdown(
+                                    id="pie_tickers",
+                                    options=options,
+                                    # upto 3 symbols will be selected on the loading (options: list of dict)
+                                    value=[op["label"] for op in options[:3]],
+                                    className="text-dark",
+                                    multi=True,
+                                ),
+                            ],
+                            className="d-inline col-md-6",
+                        ),
+                        # Div for date picker
+                        html.Div(
+                            [
+                                dcc.DatePickerSingle(
+                                    id="pie_date_picker_single",
+                                    min_date_allowed=MIN_DATE,
+                                    max_date_allowed=MAX_DATE,
+                                    date=MAX_DATE,
+                                    className="d-inline",
+                                ),
+                            ],
+                            className="d-inline col-md-6",
+                        ),
+                    ],
+                    # class for drowpdown and checklist div
                     className="bg-dark text-white",
                 ),
                 dcc.Graph(
-                    id="graph4",
+                    id="pie_chart",
                 ),
             ],
         ),
